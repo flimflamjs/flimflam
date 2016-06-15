49cf85284f56d8697bc479f551775e501b919808
Jay R Bolton <jayrbolton@gmail.com>
2016-06-15 12:45:59 -0700

    Finish notificiation

--- modules/notification/index.es6
+++ modules/notification/index.es6
@@ -1,3 +1,35 @@
-const setMsg$ = flyd.stream('')
-const hideMsg$ = flyd.flatMap(msg => flyd.delay(5000, ''), setMsg$)
+import flyd from 'flyd'
+import h from 'snabbdom/h'
+import R from 'ramda'
+flyd.flatMap = require('flyd/module/flatmap')
+flyd.afterSilence = require('flyd/module/aftersilence')
 
+// Pushes null values to the messages stream after a delay
+// pseudocode example with a 1000ms delay:
+// {[message1, 0ms], [message2, 5000ms]}
+// {[message1, 0ms], [null, 1000ms], [message2, 5000ms], [null, 6000ms]}
+
+const log = R.curryN(2, console.log.bind(console))
+
+window.flyd = flyd
+
+function init(state) {
+  state = R.merge({hideDelay: 5000, message$: flyd.stream()}, state || {})
+  // Stream of null value after a ms delay
+  // const hide$ = flyd.map(R.always(null), flyd.flatMap(() => flyd.afterSilence(state.hideDelay, flyd.stream(1)), state.message$))
+  // state.message$ = flyd.merge(state.message$, hide$)
+
+  const hide$ = flyd.map(R.always(null), flyd.flatMap(() => flyd.afterSilence(state.hideDelay, flyd.stream(1)), state.message$))
+  state.msg$ = flyd.merge(state.message$, hide$)
+
+  return state
+}
+
+
+function view(state) {
+  return h('div.ff-notification', { 
+    class: {'ff-notification--inView': state.msg$()} 
+  }, state.msg$() ? String(state.msg$()) : '' )
+}
+
+module.exports = {view, init}
--- modules/notification/index.js
+++ modules/notification/index.js
@@ -1,27 +1,50 @@
-import h from 'snabbdom/h'
-import flyd from 'flyd'
-import afterSilence from 'flyd/module/aftersilence'
-import flatMap from 'flyd/module/flatmap'
-import R from 'ramda'
+'use strict';
 
-// Pass in a stream of notification messages
-const init = (msg$, delayMS) => {
-  // Hide notification after 5s delay
-  let streams = {
-    msg: flyd.merge(msg$, flatMap(() => afterSilence(delayMS, flyd.stream(null)), msg$))
-  }
-  let updates = {
-    msg: (msg, state) => R.merge(state, {msg: msg, isShowing: Boolean(msg)})
-  }
-  let state = {msg: null, isShowing: false}
-  return { state, streams, updates }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _flyd = require('flyd');
+
+var _flyd2 = _interopRequireDefault(_flyd);
+
+var _snabbdomH = require('snabbdom/h');
+
+var _snabbdomH2 = _interopRequireDefault(_snabbdomH);
+
+var _ramda = require('ramda');
+
+var _ramda2 = _interopRequireDefault(_ramda);
+
+_flyd2['default'].flatMap = require('flyd/module/flatmap');
+_flyd2['default'].afterSilence = require('flyd/module/aftersilence');
+
+// Pushes null values to the messages stream after a delay
+// pseudocode example with a 1000ms delay:
+// {[message1, 0ms], [message2, 5000ms]}
+// {[message1, 0ms], [null, 1000ms], [message2, 5000ms], [null, 6000ms]}
+
+var log = _ramda2['default'].curryN(2, console.log.bind(console));
+
+window.flyd = _flyd2['default'];
+
+function init(state) {
+  state = _ramda2['default'].merge({ hideDelay: 5000, message$: _flyd2['default'].stream() }, state || {});
+  // Stream of null value after a ms delay
+  // const hide$ = flyd.map(R.always(null), flyd.flatMap(() => flyd.afterSilence(state.hideDelay, flyd.stream(1)), state.message$))
+  // state.message$ = flyd.merge(state.message$, hide$)
+
+  var hide$ = _flyd2['default'].map(_ramda2['default'].always(null), _flyd2['default'].flatMap(function () {
+    return _flyd2['default'].afterSilence(state.hideDelay, _flyd2['default'].stream(1));
+  }, state.message$));
+  state.msg$ = _flyd2['default'].merge(state.message$, hide$);
+
+  return state;
 }
 
+function view(state) {
+  return (0, _snabbdomH2['default'])('div.ff-notification', {
+    'class': { 'ff-notification--inView': state.msg$() }
+  }, state.msg$() ? String(state.msg$()) : '');
+}
 
-const view = comp =>
-  h('div.notification'
-  , {class: {'is-showing': comp.state.isShowing}}
-  , String(comp.state.msg ? comp.state.msg : '')
-  )
+module.exports = { view: view, init: init };
 
-module.exports = {view, init}
